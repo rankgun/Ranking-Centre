5fd752ea856530d36106486a20068858f37686fc
Noah Haf <61884850+Noah-Haf@users.noreply.github.com>
2025-04-17 17:21:21 +0100

    + Add basic types

---
+++ src/shared/types.ts
@@ -0,0 +1,38 @@
+import { t } from "@rbxts/t";
+
+enum HttpMethod {
+	GET = "GET",
+	POST = "POST",
+	DELETE = "DELETE",
+	PATCH = "PATCH",
+}
+
+interface CustomRank {
+	id: string;
+	header: string;
+	body: string;
+	rankId: number;
+	gamepass: number;
+}
+
+const tCustomRank = t.interface({
+	id: t.string,
+	header: t.string,
+	body: t.string,
+	rankId: t.number,
+	gamepass: t.number,
+});
+
+export interface RankCentre {
+	id: string;
+	lastRequest?: string;
+	ranks: CustomRank[];
+}
+
+const tRankCentre = t.interface({
+	id: t.string,
+	lastRequest: t.optional(t.string),
+	ranks: t.array(tCustomRank),
+});
+
+export { HttpMethod, tRankCentre };
